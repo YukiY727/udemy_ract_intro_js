2b7f649d5696c855d67d1afaceab036811872c34
YukiY727 <83635663+YukiY727@users.noreply.github.com>
2022-04-05 09:07:26 +0900

    変数part修了

---
+++ src/let_const.js
@@ -0,0 +1,17 @@
+
+// const, let等の変数宣言
+
+// 従来
+var vall = "var変数"
+console.log(vall);
+
+// letは再定義不可能
+// let val1 = "let変数";
+// val1 = "再代入";
+// let val1 = "再定義";
+
+// constは再代入も不可能
+// const val2 = "const変数";
+// val2 = "再代入";
+
+// constは配列、オブジェクトの中身の変更は可能
